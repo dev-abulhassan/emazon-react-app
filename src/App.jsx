1c0e1b7750bdc56ec5d15228d989be97d190451b
imadnanhassan~ <abulhassan.dev@gmail.com>
2023-03-27 04:44:18 +0600

    api data lode in shop page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import Login from './Pages/Login/Login'
+import Shop from './Pages/Shop/Shop'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Header></Header>
+      <Header />
+      <Shop />
+      <Login />
     </div>
   )
 }
